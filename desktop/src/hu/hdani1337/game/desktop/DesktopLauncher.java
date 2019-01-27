24d01f21664c3f66d7896e3ce4671fccd8b80941
Horváth Dániel <admin@hdani1337.hu>
2019-01-27 21:54:27 +0100

    Kezdőképernyő, hangok, package nevek

--- desktop/src/hu/hdani1337/game/desktop/DesktopLauncher.java
+++ desktop/src/hu/hdani1337/game/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package hu.hdani1337.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+
+import hu.hdani1337.cargame.CarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new CarGame(), config);
 	}
 }
