2712fd65703c96cd74b2b8eefd120f5db3f8c9e8
Horváth Dániel <admin@hdani1337.hu>
2019-01-27 15:20:19 +0100

    Ütközés, háttér, sávok, nehézség, vesztés képernyő

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -19,6 +19,7 @@ import com.mygdx.game.MyBaseClasses.Scene2D.MyScreen;
 import com.mygdx.game.MyBaseClasses.Scene2D.MyStage;
 import com.mygdx.game.MyBaseClasses.Scene2D.OneSpriteActor;
 import com.mygdx.game.MyBaseClasses.Scene2D.OneSpriteStaticActor;
+import com.mygdx.game.Screen.CrashScreen;
 
 import java.awt.event.KeyEvent;
 
@@ -28,20 +29,30 @@ public class GameScreen extends MyScreen {
 
 	OneSpriteStaticActor myCar;
 	OneSpriteStaticActor enemyCar;
+	OneSpriteStaticActor background;
+
+	int pontszam;
 
 
-	MyStage stage = new MyStage(new ExtendViewport(160,160), spriteBatch, game ) {
+
+	MyStage stage = new MyStage(new ExtendViewport(1280,720), spriteBatch, game ) {
 
 		int sav;
-
+		int nehezseg;
 
 		@Override
 		public void init() {
+			pontszam = 0;
+			nehezseg = 250;
+
 			myCar = new OneSpriteStaticActor(Assets.manager.get(Assets.CAR_TEXTURE)){
 				@Override
 				public void act(float delta){
 					super.act(delta);
-					setX(getX() +  Gdx.input.getAccelerometerY() * (float)0.5);
+					setX(getX() +  Gdx.input.getAccelerometerY() * 3);
+					if(myCar.getX()<=169 || myCar.getX()>=1070){
+						game.setScreen(new CrashScreen(game));
+					}
 				}
 			};
 
@@ -49,39 +60,51 @@ public class GameScreen extends MyScreen {
 				@Override
 				public void act(float delta) {
 					super.act(delta);
-					setY(getY() - delta * 75);
+					setY(getY() - delta * nehezseg);
 					if(enemyCar.getY() + enemyCar.getHeight() < 0){
 						sav = (int)(Math.random() * 4 + 1);
 						if(sav == 1){
-							enemyCar.setX(30);
+							enemyCar.setX(300);
 						}
 
 						if(sav == 2){
-							enemyCar.setX(60);
+							enemyCar.setX(500);
 						}
 
 						if(sav == 3){
-							enemyCar.setX(90);
+							enemyCar.setX(735);
 						}
 
 						if(sav == 4){
-							enemyCar.setX(120);
+							enemyCar.setX(940);
 						}
 						setY(stage.getHeight());
+						pontszam++;
+						nehezseg += 12;
 					}
 
 					if(overlaps(myCar,enemyCar) == true){
-						setCameraZoomSpeed(1);
-						setCameraZoomXY(myCar.getX(),myCar.getY(),(float)0.7);
+						game.setScreen(new CrashScreen(game));
 					}
 				}
 			};
 
-			myCar.setSize(10,24);
+			background = new OneSpriteStaticActor(Assets.manager.get(Assets.HATTER_TEXTURE)){
+				@Override
+				public void act(float delta) {
+					super.act(delta);
+				}
+			};
+
+			myCar.setSize(50,120);
 			myCar.addBaseCollisionRectangleShape();
+			myCar.setPosition(615,5);
 			enemyCar.addBaseCollisionRectangleShape();
-			enemyCar.setSize(10,19);
-			enemyCar.setPosition(40,getHeight());
+			enemyCar.setSize(50,95);
+			enemyCar.setPosition(500,getHeight());
+			background.setSize(1280,720);
+			background.setPosition(0,0);
+			addActor(background);
 			addActor(myCar);
 			addActor(enemyCar);
 		}
@@ -107,18 +130,18 @@ public class GameScreen extends MyScreen {
 	public void render(float delta) {
 		super.render(delta);
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			myCar.setX(myCar.getX() - (float)1.8);
+			myCar.setX(myCar.getX() - 6);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			myCar.setX(myCar.getX() + (float)1.8);
+			myCar.setX(myCar.getX() + 6);
 		}
+		System.out.println("Pontszám: " + pontszam);
 		stage.act(delta);
 		stage.draw();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-
 	}
 
 	@Override
